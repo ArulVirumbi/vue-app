91e0613237b762416acc4315708e50eeedd3a3cf
Arul Virumbi <arul.virumbi@gmail.com>
2023-12-02 19:30:04 +0530

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Resume from './components/resume.vue'
+
+</script>
+
+<template>
+  <main>
+    <div>Hello</div>
+    <Resume/>
+  </main>
+</template>
+
+<style scoped>
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+}
+</style>
---
+++ src/components/Resume.vue
@@ -0,0 +1,11 @@
+<template>
+    <main>
+      <div>Arul</div>
+    </main>
+</template>
+
+<script>
+export default{
+  name: 'Resume',
+}
+</script>
